4bac98155e2d813132204b47b72ad9c6a4eee304
jaeyeon23 <catblues282@gmail.com>
2020-05-30 17:40:43 +0900

    20.05.30 등록기능

---
+++ src/main/java/com/jyp/book/springboot/service/posts/PostsService.java
@@ -0,0 +1,20 @@
+package com.jyp.book.springboot.service.posts;
+
+import com.jyp.book.springboot.domain.posts.PostsRepository;
+import com.jyp.book.springboot.web.dto.PostsSaveRequestDto;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import javax.transaction.Transactional;
+
+@RequiredArgsConstructor
+@Service
+public class PostsService {
+
+    private final PostsRepository postsRepository;
+
+    @Transactional
+    public Long save(PostsSaveRequestDto requestDto){
+        return postsRepository.save(requestDto.toEntity()).getId();
+    }
+}

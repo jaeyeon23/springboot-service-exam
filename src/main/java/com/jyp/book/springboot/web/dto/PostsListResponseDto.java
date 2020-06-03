c3a18eca4b5d33a9f69952d5ff310f3300823a5a
jaeyeon23 <catblues282@gmail.com>
2020-06-04 00:06:22 +0900

    20.06.03 게시판 템플릿, 수정, 삭제

---
+++ src/main/java/com/jyp/book/springboot/web/dto/PostsListResponseDto.java
@@ -0,0 +1,21 @@
+package com.jyp.book.springboot.web.dto;
+
+import com.jyp.book.springboot.domain.posts.Posts;
+import lombok.Getter;
+
+import java.time.LocalDateTime;
+
+@Getter
+public class PostsListResponseDto {
+    private Long id;
+    private String title;
+    private String author;
+    private LocalDateTime modifiedDate;
+
+    public PostsListResponseDto(Posts entity) {
+        this.id = entity.getId();
+        this.title = entity.getTitle();
+        this.author = entity.getAuthor();
+        this.modifiedDate = entity.getModifiedDate();
+    }
+}
